74d8bb980d8a9027e9466145a876f2014e5aba04
Muhammad Usama Navid <concaption@gmail.com>
2021-02-26 12:00:31 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,152 @@
+//-----------------------------------------
+// Service Worker
+//
+// * Offline first
+// * Clears old cache on version change
+//
+//-----------------------------------------
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container
+  divInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', () => {
+  console.log('👍', 'butInstall-clicked');
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  promptEvent.userChoice.then((result) => {
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+  });
+});
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Init cache name/version
+const cacheName = 'v1';
+
+// which pages/assets do you want to cache?
+const cacheAssets = [
+  'index.html',
+  '/js/resume.min.js',
+  '/img',
+]
+
+// Install service worker
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+
+// Activate the service worker
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+
+  // Remove old caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        // look at all the cacheNames
+        cacheNames.map(cache => {
+          // if the current cache !== cacheName then delete it
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  );
+
+});
+
+
+
+// listen for fetch event (HTTP request)
+self.addEventListener('fetch', (e) => {
+  console.log('Service Worker: Fetching');
+
+  // Offline backup
+  // e.respondWith(
+  //   // if the user is online, perform a regular HTTP request
+  //   fetch(e.request)
+  //   // if the HTTP request fails (offline) then serve the assets requested from the cache
+  //   .catch(() => caches.match(e.request))
+  // )
+
+  // Offline first
+  e.respondWith(
+    // are the files requested in the cache already?
+    caches.match(e.request).then(cachedResponse => {
+      // if yes, then serve files from cache
+      if (cachedResponse) {
+        console.log('Found in cache!');
+        return cachedResponse;
+      }
+      // else do an HTTP request to the server
+      return fetch(e.request);
+    })
+  )
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
